06bae9aa011a50f6b88c833a5fbba7be38874ae9
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-12-27 11:04:00 +0800

    Remoce next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -2,21 +2,13 @@ const withPrefresh = require('@prefresh/next');
 const preact = require('preact');
 
 const withPreact = require('next-plugin-preact');
-
-const withPlugins = require('next-compose-plugins');
-
 const withPWA = require('next-pwa')
 
-module.exports = withPlugins(
-  [
-    [withPreact, { experimental: {modern: true} }],
-    [withPWA, { pwa: {dest: 'public'} }]
-  ]
-)
-
-
-// module.exports = withPreact({
-//   experimental: {
-//     modern: true,
-//   },
-// });
+module.exports = withPreact(withPWA({
+	pwa: {
+		dest: 'public'
+	},
+	experimental: {
+		modern: true
+	}
+}))
